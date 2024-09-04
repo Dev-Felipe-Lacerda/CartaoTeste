57ad3b95eed623fa0538111a2d86dcc2dc9cd4a3
Felipe Lacerda <devfelipelacerda@outlook.com>
2024-09-04 14:22:56 -0300

    Fx funcional
    Ajustes de aplicação e execução

--- src/Principal.java
+++ src/Principal.java
@@ -1,48 +1,55 @@
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.CycleMethod;
 import javafx.scene.paint.LinearGradient;
 import javafx.scene.paint.Stop;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Principal extends Application {
     private Card card;
-    private Text statusText;
     private float totalDividas;
 
     @Override
-
-    public void start(Stage primaryStage){
+    public void start(Stage primaryStage) {
         primaryStage.setTitle("Cálculo do Cartão");
 
-        //Inicializar classe
+        // Inicializar classe
         card = new Card() {};
 
-        //GridPane para layout
+        // GridPane para layout
         GridPane mainGrid = new GridPane();
         mainGrid.setPadding(new Insets(10));
         mainGrid.setHgap(10);
         mainGrid.setVgap(10);
         mainGrid.setAlignment(Pos.CENTER);
 
-        //Efeito do Fundo
+        // Efeito do Fundo
         StackPane mainLayout = new StackPane(mainGrid);
         mainLayout.setPadding(new Insets(10));
         mainLayout.setAlignment(Pos.CENTER);
 
-        //Backgroud Degradê
+        // Background Degradê
         BackgroundFill backgroundFill = new BackgroundFill(
                 new LinearGradient(
-                        0,0,1,1, true, CycleMethod.NO_CYCLE,
+                        0, 0, 1, 1, true, CycleMethod.NO_CYCLE,
                         new Stop(0, Color.web("#A241A8")),
-                        new Stop(0, Color.web("#65A538"))
+                        new Stop(1, Color.web("#65A538"))
                 ),
                 CornerRadii.EMPTY, Insets.EMPTY
         );
         mainLayout.setBackground(new Background(backgroundFill));
+
+        // Adiciona o layout à cena e exibe
+        Scene scene = new Scene(mainLayout, 800, 600);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args); // Inicia a aplicação JavaFX
     }
 }
